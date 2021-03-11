e6f4751c177c027b510c5e112f2c2b4800febef8
sopranoelfuego <aubinjaja@gmail.com>
2021-03-11 12:19:47 +0300

    setup pages

--- client/src/components/Home/Baner/Banner.js
+++ client/src/components/Home/Baner/Banner.js
@@ -1,18 +1,30 @@
-import React from 'react'
+import React,{useState} from 'react'
 import "./banner.css"
 import {Button} from "@material-ui/core"
+import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 
 const Banner = () => {
+    const [showCalendar,setShowCalendar]=useState(false)
     return (
         <div className="banner">
+            <div className="search__button__container">
+                <Button className="search__button" variant="outlined" onClick={(e)=>setShowCalendar(!showCalendar)}>
+                     <div className="tex">
+                     {showCalendar ? "hide date picker":"show date picker"}
+                         </div>     
+                 <CalendarTodayIcon/>
+                </Button>
+            </div>
              <div className="banner__info">
-             <h1>Get out and stretch 
-                 your imagination</h1>
-             <h5>plan a diffrent kind of gateway to
-                 uncover the hidden gems near you
-             </h5>
-             <Button variant="outlined">explore your nearby</Button>
+                    <h1>Get out and stretch 
+                        your imagination</h1>
+                    <h5>plan a diffrent kind of gateway to
+                        uncover the hidden gems near you
+                    </h5>
+                    <Button variant="outlined">explore your nearby</Button>
              </div>
+
+
         </div>
     )
 }
---
+++ client/src/components/Home/Baner/search/Search.js
@@ -0,0 +1,16 @@
+import React,{useState} from 'react'
+import "react-date-range/dist/styles.css"
+import "react-date-range/dist/theme/default.css"
+import "./Search.css"
+import {DateRangePicker} from "react-date-range"
+const Search = () => {
+    const[startDate,setStartDate]=useState(new Date())
+    const[EndDate,setEndDate]=useState(new Date())
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Search
